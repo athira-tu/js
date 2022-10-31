37b596c47f517c5a426eb4ff46b8eb99d147036b
athira <aathuathira700@gmail.com>
2022-10-31 08:51:16 +0530

    learned variable declaration

---
+++ 01variables.js
@@ -0,0 +1,9 @@
+console.log("external js file");
+var a = 10;//both re-declaration and re-assigning
+var a = "athira";
+console.log(a);
+let b = "athira";//re-assigning only
+b = 4
+console.log(b);
+const c = "hello";
+console.log(typeof (c));
