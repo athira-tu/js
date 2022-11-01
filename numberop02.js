88802993f883ee3ab1732f0c3c402bf640fa172a
athira <aathuathira700@gmail.com>
2022-11-01 08:17:46 +0530

    completed number operation

---
+++ numberop02.js
@@ -0,0 +1,27 @@
+// console.log("number operation");
+console.log(30 / 10);
+console.log(30 * 10);
+let a = 40;
+let b = 10;
+console.log(a / b);
+console.log(a * b);
+console.log(a + b);
+console.log(a - b);
+console.log(a % b)
+a++
+console.log(a++)
+console.log(++a)
+let num1 = 10;
+let num2 = 20;
+let n = num1++;
+console.log(n)
+let m = ++num2
+console.log(m);
+console.log(num1 + n)
+console.log(num2 + m)
+let ab = 10;
+console.log(ab += 5);//ab=10
+console.log(ab -= 5);//ab=15
+console.log(ab *= 5);//ab=50
+console.log(ab /= 5);//ab=10
+
