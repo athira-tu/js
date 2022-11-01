07c31e6897e8d9b070834ae360c6055085ac8c71
athira <aathuathira700@gmail.com>
2022-11-01 09:07:05 +0530

    completed string operation

---
+++ string03.js
@@ -0,0 +1,14 @@
+// console.log("hello")
+let a = "hello " + "world";
+console.log(a)
+let s1 = "hello";
+let s2 = "world";
+console.log(s1 + s2);
+console.log(s1[2]);//for printing one element
+let s3 = "welcome to the world of javascript";
+console.log(s3.length);
+console.log(s3[s3.length - 1]);
+let arr = s1.split("");
+console.log(arr);
+let arr2 = s3.split(" ");
+console.log(arr2);
