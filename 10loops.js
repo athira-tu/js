01633710e922dba3e61f7ea9648ca4928a8e284b
athira <aathuathira700@gmail.com>
2022-11-10 09:04:36 +0530

    while and do while loop is completed

---
+++ 10loops.js
@@ -0,0 +1,34 @@
+// function printUpTo(limit) {
+//     let a = 1
+//     while (a <= limit) {
+//         console.log(a)
+//         a++
+//     }
+// }
+// printUpTo(17)
+// function printUpToDowhile(limit) {
+//     let a = 1
+//     do {
+//         console.log(a)
+//         a++
+//     } while (a <= limit)
+
+
+// }
+// printUpTo(10)
+// function printUpToFor(limit) {
+//     for (let i = 0; i <= limit; i++) {
+//         console.log(i)
+//     }
+// }
+// printUpToFor(5)
+// function printFrom(limit) {
+//     for (let i = limit; i >= 1; i--)
+//         console.log(i)
+// }
+// printFrom(6)
+function printarray(arr) {
+    for (let i = 0; i < arr.length; i++)
+        console.log(arr[i])
+}
+printarray([1, 2, 3, 4, 5, 1])
